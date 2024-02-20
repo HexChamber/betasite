fac5ace5dfac78e17fe20df2559d144a28997c80
nullchamber <131722143+nullchamber@users.noreply.github.com>
2024-02-20 00:38:20 -0600

    images model

--- betasite/betasite/settings.py
+++ betasite/betasite/settings.py
@@ -45,6 +45,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'images.apps.ImagesConfig',
     'bulma',
     'social_django',
     'django_extensions',
@@ -157,7 +158,7 @@ SOCIAL_AUTH_PIPELINE = [
     'social_core.pipeline.social_auth.social_user',
     'social_core.pipeline.user.get_username',
     'social_core.pipeline.user.create_user',
-    ''
+    'account.authentication.create_profile',
     'social_core.pipeline.social_auth.associate_user',
     'social_core.pipeline.social_auth.load_extra_data',
     'social_core.pipeline.user.user_details',
