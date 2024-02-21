9727da21d4412552d4710e47f8c77955c1874fe8
nullchamber <131722143+nullchamber@users.noreply.github.com>
2024-02-21 04:33:23 -0600

    infinite pagination

--- betasite/images/views.py
+++ betasite/images/views.py
@@ -1,4 +1,5 @@
-from django.http import JsonResponse
+from django.http import JsonResponse, HttpResponse
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.views.decorators.http import require_POST
 from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.decorators import login_required
@@ -8,6 +9,40 @@ from images.models import Image
 
 
 # Create your views here.
+
+@login_required
+def image_list(request):
+    images = Image.objects.all()
+    paginator = Paginator(images, 8)
+    page = request.GET.get('page')
+    images_only = request.GET.get('images_only')
+    try:
+        images = paginator.page(page)
+    except PageNotAnInteger:
+        images = paginator.page(1)
+    except EmptyPage:
+        if images_only:
+            return HttpResponse('')
+
+        images = paginator.page(paginator.num_pages)
+    if images_only:
+        return render(
+            request,
+            'images/image/list_images.html',
+            {
+                'section': 'images',
+                'images': images
+            }
+        )
+    return render(
+        request,
+        'images/image/list.html',
+        {
+            'section': 'images',
+            'images': images
+        }
+    )
+
 
 @login_required
 def image_create(request):
@@ -37,6 +72,7 @@ def image_create(request):
     )
 
 
+@login_required
 def image_detail(request, id, slug):
     image = get_object_or_404(
         Image,
